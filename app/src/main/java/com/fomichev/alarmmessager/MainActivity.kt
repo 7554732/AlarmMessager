753b8429b95d1ee8cc6044e63484d2772b161f8c
Sergey Fomichev <7554732@gmail.com>
2023-01-13 13:55:27 +0300

    move MainPagerScreen to the separate file .5

--- app/src/main/java/com/fomichev/alarmmessager/MainActivity.kt
+++ app/src/main/java/com/fomichev/alarmmessager/MainActivity.kt
@@ -7,26 +7,12 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.fomichev.alarmmessager.ui.MainPagerScreen
 import com.fomichev.alarmmessager.ui.theme.AlarmMessagerTheme
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +23,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AlarmMessagerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -56,73 +41,5 @@ class MainActivity : ComponentActivity() {
         val pIntent = PendingIntent.getBroadcast(this, 0, intent, PENDING_INTENT_FLAG_IMMUTABLE)
         val am = getSystemService(ALARM_SERVICE) as AlarmManager
         am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 4000, pIntent)
-    }
-}
-
-@OptIn(ExperimentalPagerApi::class)
-@Composable
-fun MainPagerScreen(onStart: () -> Unit) {
-    val tabData = listOf(
-        "MUSIC" to Icons.Filled.Home,
-        "MARKET" to Icons.Filled.ShoppingCart,
-        "FILMS" to Icons.Filled.AccountBox,
-        "BOOKS" to Icons.Filled.Settings,
-    )
-    val pagerState = rememberPagerState(
-        pageCount = tabData.size,
-        initialOffscreenLimit = 2,
-        infiniteLoop = true,
-        initialPage = 1,
-    )
-    val tabIndex = pagerState.currentPage
-    val coroutineScope = rememberCoroutineScope()
-    Column {
-        TabRow(
-            selectedTabIndex = tabIndex,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                )
-            }
-        ) {
-            tabData.forEachIndexed { index, pair ->
-                Tab(selected = tabIndex == index, onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                }, text = {
-                    Text(text = pair.first)
-                }, icon = {
-                    Icon(imageVector = pair.second, contentDescription = null)
-                })
-            }
-        }
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.weight(1f)
-        ) { index ->
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = tabData[index].first,
-                )
-                Button(
-                    onClick = onStart
-                ){
-                    Text(text = "Stat")
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AlarmMessagerTheme {
-        MainPagerScreen({})
     }
 }
