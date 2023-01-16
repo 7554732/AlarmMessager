fac9d8188aa67ad2e260852c4744aaba4fb045d2
Sergey Fomichev <7554732@gmail.com>
2023-01-16 10:49:54 +0300

    add CircleStartButton

--- app/src/main/java/com/fomichev/alarmmessager/ui/StartScreen.kt
+++ app/src/main/java/com/fomichev/alarmmessager/ui/StartScreen.kt
@@ -2,13 +2,21 @@ package com.fomichev.alarmmessager.ui
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fomichev.alarmmessager.R
 
@@ -49,10 +57,38 @@ fun StartScreen(onStart: () -> Unit) {
                 })
             }
         }
-        Button(
-            onClick = onStart
-        ) {
-            Text(text = stringResource(R.string.start))
-        }
+        CircleStartButton(onStart = {})
     }
+}
+
+
+@Composable
+fun CircleStartButton(
+    modifier: Modifier = Modifier,
+    isStartedState: MutableState<Boolean> = remember { mutableStateOf(false) },
+    onStart: (Boolean) -> Unit
+){
+    Button(
+        modifier = modifier.size(150.dp),
+        shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+        onClick = {
+            isStartedState.value = !isStartedState.value
+            onStart(isStartedState.value)
+        }
+    ) {
+        Text(
+            text = if(!isStartedState.value)
+                stringResource(R.string.start)
+            else
+                stringResource(R.string.stop) ,
+            color = Color.White
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun Preview() {
+    CircleStartButton(onStart = {})
 }
