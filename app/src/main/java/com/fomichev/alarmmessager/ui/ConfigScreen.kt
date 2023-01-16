1e24e6b1e904bde3c96fc623eb50b375a19b6b03
Sergey Fomichev <7554732@gmail.com>
2023-01-16 18:22:04 +0300

    add PhoneField .5

--- app/src/main/java/com/fomichev/alarmmessager/ui/ConfigScreen.kt
+++ app/src/main/java/com/fomichev/alarmmessager/ui/ConfigScreen.kt
@@ -1,12 +1,23 @@
 package com.fomichev.alarmmessager.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ConfigScreen() {
@@ -15,6 +26,105 @@ fun ConfigScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "ConfigScreen")
+        LoginScreen()
     }
+}
+
+@Composable
+fun LoginScreen() {
+    var phoneNumber by rememberSaveable { mutableStateOf("") }
+    Column {
+        PhoneField(phoneNumber,
+            mask = "000 000 00 00",
+            maskNumber = '0',
+            onPhoneChanged = { phoneNumber = it })
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        PhoneField(phoneNumber,
+            mask = "(000) 000 00 00",
+            maskNumber = '0',
+            onPhoneChanged = { phoneNumber = it })
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        PhoneField(phoneNumber,
+            mask = "+7-000-000-00-00",
+            maskNumber = '0',
+            onPhoneChanged = { phoneNumber = it })
+    }
+}
+
+@Composable
+fun PhoneField(
+    phone: String,
+    modifier: Modifier = Modifier,
+    mask: String = "000 000 00 00",
+    maskNumber: Char = '0',
+    onPhoneChanged: (String) -> Unit
+) {
+    TextField(
+        value = phone,
+        onValueChange = { it ->
+            onPhoneChanged(it.take(mask.count { it == maskNumber }))
+        },
+        label = {
+            Text(text = "Phone number")
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+        visualTransformation = PhoneVisualTransformation(mask, maskNumber),
+        modifier = modifier.fillMaxWidth(),
+    )
+}
+
+class PhoneVisualTransformation(val mask: String, val maskNumber: Char) : VisualTransformation {
+
+    private val maxLength = mask.count { it == maskNumber }
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val trimmed = if (text.length > maxLength) text.take(maxLength) else text
+
+        val annotatedString = buildAnnotatedString {
+            if (trimmed.isEmpty()) return@buildAnnotatedString
+
+            var maskIndex = 0
+            var textIndex = 0
+            while (textIndex < trimmed.length && maskIndex < mask.length) {
+                if (mask[maskIndex] != maskNumber) {
+                    val nextDigitIndex = mask.indexOf(maskNumber, maskIndex)
+                    append(mask.substring(maskIndex, nextDigitIndex))
+                    maskIndex = nextDigitIndex
+                }
+                append(trimmed[textIndex++])
+                maskIndex++
+            }
+        }
+
+        return TransformedText(annotatedString, PhoneOffsetMapper(mask, maskNumber))
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PhoneVisualTransformation) return false
+        if (mask != other.mask) return false
+        if (maskNumber != other.maskNumber) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return mask.hashCode()
+    }
+}
+
+private class PhoneOffsetMapper(val mask: String, val numberChar: Char) : OffsetMapping {
+
+    override fun originalToTransformed(offset: Int): Int {
+        var noneDigitCount = 0
+        var i = 0
+        while (i < offset + noneDigitCount) {
+            if (mask[i++] != numberChar) noneDigitCount++
+        }
+        return offset + noneDigitCount
+    }
+
+    override fun transformedToOriginal(offset: Int): Int =
+        offset - mask.take(offset).count { it != numberChar }
 }
