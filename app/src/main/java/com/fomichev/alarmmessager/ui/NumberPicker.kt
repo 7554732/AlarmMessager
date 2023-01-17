d81b78b6bea596fc74d786842987fa184bb93f14
Sergey Fomichev <7554732@gmail.com>
2023-01-17 17:45:08 +0300

    add setValue() to NumberPicker

--- app/src/main/java/com/fomichev/alarmmessager/ui/NumberPicker.kt
+++ app/src/main/java/com/fomichev/alarmmessager/ui/NumberPicker.kt
@@ -24,6 +24,7 @@ fun NumberPicker(
                     }
                     minValue = min
                     maxValue = max
+                    setValue(value)
                 }
         }
     )
