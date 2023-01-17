c30da0c5c0e8a221e5b23a1ce19692c73143d7cf
Sergey Fomichev <7554732@gmail.com>
2023-01-17 18:16:29 +0300

    add AlarmMessagerApplication 1

---
+++ app/src/main/java/com/fomichev/alarmmessager/AlarmMessagerApplication.java
@@ -0,0 +1,9 @@
+package com.fomichev.alarmmessager;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class AlarmMessagerApplication extends Application {
+}
