a58ec782a1157dd99438e9f0d5f065dff90f348c
Sergey Fomichev <7554732@gmail.com>
2023-01-17 14:37:56 +0300

    add onStart(), onMsgSave() in AlarmMessagerViewModel

--- app/src/main/java/com/fomichev/alarmmessager/ui/AlarmMessagerViewModel.kt
+++ app/src/main/java/com/fomichev/alarmmessager/ui/AlarmMessagerViewModel.kt
@@ -1,7 +1,21 @@
 package com.fomichev.alarmmessager.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.fomichev.alarmmessager.domain.AlarmCFG
+import com.fomichev.alarmmessager.domain.Msg
 
 class AlarmMessagerViewModel: ViewModel() {
+    var alarmCfg = AlarmCFG()
+    var msg = Msg()
 
+    fun onStart(_alarmCfg: AlarmCFG) {
+        alarmCfg = _alarmCfg
+        Log.d("AlarmMessagerViewModel ", " " + alarmCfg)
+    }
+
+    fun onMsgSave(_msg: Msg) {
+        msg = _msg
+        Log.d("AlarmMessagerViewModel ", " " + msg)
+    }
 }
