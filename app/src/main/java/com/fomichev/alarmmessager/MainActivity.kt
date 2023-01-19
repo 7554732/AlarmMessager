a318bdb40d473c0bad16fab987065d619c8d6480
Sergey Fomichev <7554732@gmail.com>
2023-01-19 12:51:52 +0300

    add datastore-preferences 5

--- app/src/main/java/com/fomichev/alarmmessager/MainActivity.kt
+++ app/src/main/java/com/fomichev/alarmmessager/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.fomichev.alarmmessager
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Intent
-import android.os.Build
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -12,13 +9,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.fomichev.alarmmessager.ui.MainPagerScreen
 import com.fomichev.alarmmessager.ui.theme.AlarmMessagerTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.first
+
+val Context.dataStore by preferencesDataStore(name = "settings")
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    val EXAMPLE_COUNTER = intPreferencesKey("example_counter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,6 +39,23 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val exampleCounterFlow: Flow<Int> = dataStore.data
+            .map { preferences ->
+                // No type safety.
+                preferences[EXAMPLE_COUNTER] ?: 0
+            }
+
+        lifecycleScope.launch{
+            incrementCounter()
+            val exampleData = dataStore.data.first()
+            Log.d("exampleData ", "" + exampleData)
+        }
     }
 
+    suspend fun incrementCounter() {
+        dataStore.edit { settings ->
+            val currentCounterValue = settings[EXAMPLE_COUNTER] ?: 0
+            settings[EXAMPLE_COUNTER] = currentCounterValue + 1
+        }
+    }
 }
