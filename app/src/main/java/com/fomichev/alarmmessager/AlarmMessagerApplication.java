d581efc37041952c2ee9fb89e73a47146d07507e
Sergey Fomichev <7554732@gmail.com>
2023-01-19 12:11:37 +0300

    rename AlarmMessagerApplication java to kt

--- app/src/main/java/com/fomichev/alarmmessager/AlarmMessagerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.fomichev.alarmmessager;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class AlarmMessagerApplication extends Application {
-}
---
+++ app/src/main/java/com/fomichev/alarmmessager/AlarmMessagerApplication.kt
@@ -0,0 +1,10 @@
+package com.fomichev.alarmmessager
+
+import android.app.Application
+import android.content.Context
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+class AlarmMessagerApplication: Application() {
+}
