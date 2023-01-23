9df7323e07d3d129b933292be71ccc6f33e47b49
Sergey Fomichev <7554732@gmail.com>
2023-01-23 19:32:37 +0300

    add setStarted() by goAsync() to the AlarmReceiver 8

--- app/src/main/java/com/fomichev/alarmmessager/AlarmReceiver.kt
+++ app/src/main/java/com/fomichev/alarmmessager/AlarmReceiver.kt
@@ -4,9 +4,33 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.fomichev.alarmmessager.repository.SettingsRepository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AlarmReceiver: BroadcastReceiver() {
+
+    private val scope = CoroutineScope(SupervisorJob())
+
+    @Inject
+    lateinit var settingsRepository: SettingsRepository
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("AlarmReceiver", "onReceive");
+        val pendingResult: PendingResult = goAsync()
+
+        scope.launch(Dispatchers.Default) {
+            try {
+                settingsRepository.setStarted(false)
+                Log.d("AlarmReceiver", "onReceive")
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 }
