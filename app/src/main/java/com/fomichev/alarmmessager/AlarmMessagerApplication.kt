23ca0b32baaede4197427ada38dff9d9c1e2bb7a
Sergey Fomichev <7554732@gmail.com>
2023-01-29 13:01:08 +0300

    add Timber

--- app/src/main/java/com/fomichev/alarmmessager/AlarmMessagerApplication.kt
+++ app/src/main/java/com/fomichev/alarmmessager/AlarmMessagerApplication.kt
@@ -4,7 +4,26 @@ import android.app.Application
 import android.content.Context
 
 import dagger.hilt.android.HiltAndroidApp;
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltAndroidApp
 class AlarmMessagerApplication: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+
+        delayedInit()
+    }
+    private fun delayedInit() {
+        if (BuildConfig.DEBUG) {
+            applicationScope.launch {
+                Timber.plant(Timber.DebugTree())
+            }
+        }
+    }
 }
